92752d0058b6dcc9d674b596b2fcec08a6c51f3b
jay.park <jay.park@tosslab.com>
2022-05-28 17:19:39 +0900

    refactoring CountBits

--- app/src/test/java/com/example/myapplication/primitives/CountBits.java
+++ app/src/test/java/com/example/myapplication/primitives/CountBits.java
@@ -1,5 +1,8 @@
 package com.example.myapplication.primitives;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CountBits {
     public static short countBits(int x) {
         short numbBits = 0;
@@ -8,5 +11,16 @@ public class CountBits {
             x >>>= 1;
         }
         return numbBits;
+    }
+
+    @Test
+    public void testCountBits() {
+        short test01 = 0x100;
+        System.out.println(" test01 " + test01);
+        Assert.assertEquals(CountBits.countBits(test01), 1);
+
+        short test02 = 0x1011;
+        System.out.println(" test02 " + test02);
+        Assert.assertEquals(CountBits.countBits(test02), 3);
     }
 }
